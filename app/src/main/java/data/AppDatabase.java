ad9e628725a1f2368a0552fe46fca47b753dd67d
getsadzeg <getsadzeguram@gmail.com>
2019-05-15 20:45:40 +0400

    getting instance of prepopulated database

--- app/src/main/java/data/AppDatabase.java
+++ app/src/main/java/data/AppDatabase.java
@@ -1,4 +1,49 @@
 package data;
 
-public class AppDatabase {
+import android.content.Context;
+import android.util.Log;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.room.migration.Migration;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+import data.dao.UserDAO;
+import data.dao.WordsDAO;
+import data.model.UserEntry;
+import data.model.WordEntry;
+
+@Database(entities = {WordEntry.class, UserEntry.class}, version = 2, exportSchema = false)
+//set exportSchema to 'true' if we want to export db to folder
+public abstract class AppDatabase extends RoomDatabase {
+
+    private static final String LOG_TAG = AppDatabase.class.getSimpleName();
+    private static final Object LOCK = new Object();
+    static final String DATABASE_NAME = "barbare.sqlite";
+    private static AppDatabase sInstance;
+
+    public static AppDatabase getInstance(Context context) {
+        if (sInstance == null) {
+            synchronized (LOCK) {
+                Log.d(LOG_TAG, "Creating new database instance");
+                sInstance = Room.databaseBuilder(context.getApplicationContext(),
+                        AppDatabase.class, AppDatabase.DATABASE_NAME)
+                        .addMigrations(MIGRATION_1_2)
+                        .allowMainThreadQueries() //temporary
+                        .build();
+            }
+        }
+        Log.d(LOG_TAG, "Getting the database instance");
+        return sInstance;
+    }
+
+    static final Migration MIGRATION_1_2 = new Migration(1, 2) {
+        @Override
+        public void migrate(SupportSQLiteDatabase database) {
+            Log.d(LOG_TAG, "in migrate()"); //doesn't log??
+        }
+    };
+
+    public abstract WordsDAO wordsDAO();
+    public abstract UserDAO userDAO();
 }
--- app/src/main/java/data/DatabaseCopier.java
+++ app/src/main/java/data/DatabaseCopier.java
@@ -1,4 +1,76 @@
 package data;
 
+import android.content.Context;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 public class DatabaseCopier {
+    private static final String TAG = DatabaseCopier.class.getSimpleName();
+
+    private AppDatabase mAppDataBase;
+    private static Context appContext;
+
+    private static class Holder {
+        private static final DatabaseCopier INSTANCE = new DatabaseCopier();
+    }
+
+    public static DatabaseCopier getInstance(Context context) {
+        appContext = context;
+        return Holder.INSTANCE;
+    }
+
+    private DatabaseCopier() {
+        //call method that check if database not exists and copy prepopulated file from assets
+        copyDatabaseFromAssets(appContext, AppDatabase.DATABASE_NAME, false);
+        mAppDataBase = AppDatabase.getInstance(appContext);
+    }
+
+    public AppDatabase getRoomDatabase() {
+        return mAppDataBase;
+    }
+
+
+    private boolean copyDatabaseFromAssets(Context context, String databaseName , boolean overwrite)  {
+
+        File outputFile = context.getDatabasePath(databaseName);
+        if (outputFile.exists() && !overwrite) {
+            return true;
+        }
+
+        outputFile = context.getDatabasePath(databaseName + ".temp");
+        outputFile.getParentFile().mkdirs();
+
+        try {
+            InputStream inputStream = context.getAssets().open(databaseName);
+            OutputStream outputStream = new FileOutputStream(outputFile);
+
+
+            // transfer bytes from the input stream into the output stream
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = inputStream.read(buffer)) > 0) {
+                outputStream.write(buffer, 0, length);
+            }
+
+            // Close the streams
+            outputStream.flush();
+            outputStream.close();
+            inputStream.close();
+
+            outputFile.renameTo(context.getDatabasePath(databaseName));
+
+        } catch (IOException e) {
+            if (outputFile.exists()) {
+                outputFile.delete();
+            }
+            return false;
+        }
+
+        return true;
+    }
+
 }
